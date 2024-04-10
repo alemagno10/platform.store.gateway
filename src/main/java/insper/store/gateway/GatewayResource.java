dd82c5e7d7d77d215e3dc6b38604c2e214f6fd5c
alemagno10 <alemagno03@gmail.com>
2024-04-10 13:09:32 -0300

    remove: requestmapping

--- src/main/java/insper/store/gateway/GatewayResource.java
+++ src/main/java/insper/store/gateway/GatewayResource.java
@@ -1,11 +1,9 @@
 package insper.store.gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class GatewayResource {
 
     @GetMapping("/hello")
