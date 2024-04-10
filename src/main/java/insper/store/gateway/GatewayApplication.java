2458a5ef3936bc254c3fc3b1df7dc9b1f22f2a5b
alemagno10 <alemagno03@gmail.com>
2024-04-10 10:12:15 -0300

    remove: swagger

--- src/main/java/insper/store/gateway/GatewayApplication.java
+++ src/main/java/insper/store/gateway/GatewayApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableSwagger2
+// @EnableSwagger2
 public class GatewayApplication {
 
     public static void main(String[] args) {
